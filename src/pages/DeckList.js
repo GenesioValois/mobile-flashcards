fcc35cb64fa10396ec162f667ee6d4d2addf9618
Genésio Valois <genas96@gmail.com>
2021-04-19 20:03:00 +0100

    feat: Add fab to create Deck

--- src/pages/DeckList.js
+++ src/pages/DeckList.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { ScrollView, SafeAreaView, View, StyleSheet } from "react-native";
-import { Card, Title, Paragraph } from "react-native-paper";
+import { Card, Title, Paragraph, FAB } from "react-native-paper";
 import { useSelector } from "react-redux";
 import Icon from "react-native-vector-icons/FontAwesome";
+
 const myIcon = () => <Icon name="folder" size={30} />;
 
 const DeckList = ({ navigation }) => {
@@ -33,6 +34,11 @@ const DeckList = ({ navigation }) => {
           })}
         </View>
       </ScrollView>
+      <FAB
+        style={styles.fab}
+        icon="plus"
+        onPress={() => navigation.navigate("New Deck", { decksTotal: deckIds.length })}
+      />
     </SafeAreaView>
   );
 };
@@ -44,6 +50,13 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     marginVertical: 10,
     backgroundColor: "white",
+  },
+  fab: {
+    position: "absolute",
+    marginHorizontal: 20,
+    marginVertical: 25,
+    right: 0,
+    bottom: 0,
   },
   text: { color: "black" },
 });
