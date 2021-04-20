8987d81356c36eec7de2d001d961103e782d3256
Genésio Valois <genas96@gmail.com>
2021-04-20 19:58:20 +0100

    feat: Add notification register and schedule

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Provider as PaperProvider } from "react-native-paper";
 import { Provider as StoreProvider } from "react-redux";
 import store from "./store";
 import Layout from "./components/Layout";
+import { setLocalNotification, schedulePushNotification } from "./notification";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+  useEffect(() => {
+    setLocalNotification()
+      .then((token) => setExpoPushToken(token))
+      .catch((error) => console.log(error));
+    notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
+      setNotification(notification);
+    });
+    schedulePushNotification();
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   return (
     <StoreProvider store={store}>
       <PaperProvider>
